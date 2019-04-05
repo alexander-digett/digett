1289c20363c9db957ec6684e21b467c09e6d5460
alexander-digett <alexander@digett.com>
2019-04-04 19:10:03 -0500

    assets name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,3 @@
-var netlifyCmsPaths = {
-  resolve: `gatsby-plugin-netlify-cms-paths`,
-  options: {
-    cmsConfig: `/static/admin/config.yml`,
-  },
-}
-
 module.exports = {
   siteMetadata: {
     title: `Digett`,
@@ -49,7 +42,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    netlifyCmsPaths,
     {
       resolve: `@gatsby-contrib/gatsby-plugin-elasticlunr-search`,
       options: {
@@ -65,20 +57,6 @@ module.exports = {
           },
         },
       },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `assets`,
-        path: `${__dirname}/static/assets`
-      }
     },
     {
       resolve: `gatsby-plugin-google-tagmanager`,
@@ -98,18 +76,25 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: `assets`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
+        name: `pages`,
       },
     },
+    `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
-          netlifyCmsPaths,
           {
             resolve: 'gatsby-remark-relative-images',
             options: {
@@ -135,49 +120,11 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              // Class prefix for <pre> tags containing syntax highlighting;
-              // defaults to 'language-' (eg <pre class="language-js">).
-              // If your site loads Prism into the browser at runtime,
-              // (eg for use with libraries like react-live),
-              // you may use this to prevent Prism from re-processing syntax.
-              // This is an uncommon use-case though;
-              // If you're unsure, it's best to use the default value.
-              classPrefix: "language-",
-              // This is used to allow setting a language for inline code
-              // (i.e. single backticks) by creating a separator.
-              // This separator is a string and will do no white-space
-              // stripping.
-              // A suggested value for English speakers is the non-ascii
-              // character '›'.
-              inlineCodeMarker: null,
-              // This lets you set up language aliases.  For example,
-              // setting this to '{ sh: "bash" }' will let you use
-              // the language "sh" which will highlight using the
-              // bash highlighter.
-              aliases: {},
-              // This toggles the display of line numbers globally alongside the code.
-              // To use it, add the following line in src/layouts/index.js
-              // right after importing the prism color scheme:
-              //  `require("prismjs/plugins/line-numbers/prism-line-numbers.css");`
-              // Defaults to false.
-              // If you wish to only show line numbers on certain code blocks,
-              // leave false and use the {numberLines: true} syntax below
-              showLineNumbers: false,
-              // If setting this to true, the parser won't handle and highlight inline
-              // code used in markdown i.e. single backtick code like `this`.
-              noInlineHighlight: false,
-            },
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-sharp`,
+			resolve: "gatsby-plugin-netlify-cms",
+			options: {
+				modulePath: `${__dirname}/src/cms/cms.js`
+			}
+		},
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -204,7 +151,6 @@ module.exports = {
     `gatsby-plugin-lodash`,
     `gatsby-plugin-glamor`,
     `gatsby-plugin-sitemap`,
-    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-netlify`, // make sure to keep it last in the array
     `gatsby-plugin-netlify-headers`,
     
